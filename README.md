70a6c51ccd4df33452d27490e55da5f1bfa9681f
Will C <w.chou06@gmail.com>
2017-04-18 19:37:22 -0400

    Removed unused functions, updated readme and package.json

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
-Reddit restful routing using Koa.js library
+# Reddit Backend for ClueP Dev Challenge
 
+## Requirements: 
+Develop a simple Reddit client web UI.
+
+The Index page should have a simple but efficient AutoComplete box which takes you to the appropriate subreddit, you must handle the case when the subreddit doesn’t exist.
+
+Each Subreddit page should have at least the following
+Jumbotron displaying the subreddit and Banner (if it exists)
+A max of 25 posts with the ability to load 25 more by click on a “Load More” button.
+Each Post must have a title, link to post, and a pic (if it exists)
+Each Post page should have at least the following
+Jumbotron displaying the subreddit
+Title of post and post content
+and all comments in whatever manner you may like
+
+## Approach:
+Accessed Reddit API using RESTful routing built with Koa.js libraries.
 Tested during development using postman.
+
+## Local Installation:
+
+1. run `git clone git@github.com:wichopy/reddit-client-backend.git` at your desired location to clone repo.
+2. `npm i` to get dependancies
+  - `npm install -g nodemon` to run server. Use for hot reloading on changes.
+3. `npm start` to run server and the api will be accessible on `localhost:3001`
+
+
--- index.js
+++ index.js
@@ -12,100 +12,92 @@ app.use(cors());
 //body parser
 app.use(bodyParser());
 app.use(async(ctx, next) => {
-  // the parsed body will store in ctx.request.body
-  // if nothing was parsed, body will be an empty object {}
-  // console.log('found this in the body: ', ctx.request)
-  ctx.body = ctx.request.body;
-  await next();
+    // the parsed body will store in ctx.request.body
+    // if nothing was parsed, body will be an empty object {}
+    // console.log('found this in the body: ', ctx.request)
+    ctx.body = ctx.request.body;
+    await next();
 });
 // x-response-time
 app.use(async function(ctx, next) {
-  const start = new Date();
-  await next();
-  const ms = new Date() - start;
-  ctx.set('X-Response-Time', `${ms}ms`);
+    const start = new Date();
+    await next();
+    const ms = new Date() - start;
+    ctx.set('X-Response-Time', `${ms}ms`);
 });
 //  logger
 app.use(async function(ctx, next) {
-  const start = new Date();
-  await next();
-  const ms = new Date() - start;
-  console.log(`${ctx.method} ${ctx.url} - ${ms}`);
+    const start = new Date();
+    await next();
+    const ms = new Date() - start;
+    console.log(`${ctx.method} ${ctx.url} - ${ms}`);
 });
 //**end of middleware
+
 const reddit = {
-  frontpage: async(ctx) => {
-    console.log('get frontpage')
-    const fp = await rp('https://www.reddit.com/r/frontpage.json')
-    const fpParsed = JSON.parse(fp)
-    ctx.body = fpParsed
-  },
+    frontpage: async(ctx) => {
+        console.log('get frontpage')
+        const fp = await rp('https://www.reddit.com/r/frontpage.json')
+        const fpParsed = JSON.parse(fp)
+        ctx.body = fpParsed
+    },
 
-  subreddit: async(ctx, subreddit) => {
-    try {
-      console.log('getting subreddit..')
-      const sr = await rp(`https://www.reddit.com/r/${subreddit}.json`)
-      const srParsed = JSON.parse(sr);
-      const srInfo = await rp(`https://www.reddit.com/r/${subreddit}/about.json`)
-      const srInfoParsed = JSON.parse(srInfo);
-      ctx.body.sr = srParsed;
-      ctx.body.srInfo = srInfoParsed;
-    } catch (error) {
-      ctx.body = `subreddit ${subreddit} does not exist`
-    }
-  },
-  subredditNextPage: async(ctx, subreddit, afterCode) => {
-    console.log('in next page')
-    console.log(subreddit)
+    subreddit: async(ctx, subreddit) => {
+        try {
+            console.log('getting subreddit..')
+            const sr = await rp(`https://www.reddit.com/r/${subreddit}.json`)
+            const srParsed = JSON.parse(sr);
+            const srInfo = await rp(`https://www.reddit.com/r/${subreddit}/about.json`)
+            const srInfoParsed = JSON.parse(srInfo);
+            ctx.body.sr = srParsed;
+            ctx.body.srInfo = srInfoParsed;
+        } catch (error) {
+            ctx.body = `subreddit ${subreddit} does not exist`
+        }
+    },
+    subredditNextPage: async(ctx, subreddit, afterCode) => {
+        console.log('in next page')
+        console.log(subreddit)
 
-    console.log(afterCode)
-    const nextPage = await rp(`https://www.reddit.com/r/${subreddit}.json?after=${afterCode}&count=25`)
-    const nextPageParsed = JSON.parse(nextPage);
-    ctx.body.sr = nextPageParsed;
-  },
-  subredditPrevPage: async(ctx, subreddit, beforeCode) => {
-    console.log('in prev page')
-    console.log(subreddit)
-
-    console.log(beforeCode)
-    const prevPage = await rp(`https://www.reddit.com/r/${subreddit}.json?before=${beforeCode}&count=25`)
-    const prevPageParsed = JSON.parse(prevPage);
-    ctx.body.sr = prevPageParsed;
-  },
-  posts: async(ctx) => {
-    try {
-      console.log('getting posts.. at permakink', ctx.body.permalink)
-      const removeLastChar = ctx.body.permalink.slice(0, -1);
-      console.log(`https://www.reddit.com${removeLastChar}.json`)
-      const postComments = await rp(`https://www.reddit.com${removeLastChar}.json`)
-      const postCommentsParsed = JSON.parse(postComments);
-      ctx.body = postCommentsParsed;
-      // console.log(postCommentsParsed)
-    } catch (error) {
-      ctx.body = `subreddit ${subreddit} does not exist`
+        console.log(afterCode)
+        const nextPage = await rp(`https://www.reddit.com/r/${subreddit}.json?after=${afterCode}&count=25`)
+        const nextPageParsed = JSON.parse(nextPage);
+        ctx.body.sr = nextPageParsed;
+    },
+    posts: async(ctx) => {
+        try {
+            console.log('getting posts.. at permakink', ctx.body.permalink)
+            const removeLastChar = ctx.body.permalink.slice(0, -1);
+            console.log(`https://www.reddit.com${removeLastChar}.json`)
+            const postComments = await rp(`https://www.reddit.com${removeLastChar}.json`)
+            const postCommentsParsed = JSON.parse(postComments);
+            ctx.body = postCommentsParsed;
+            // console.log(postCommentsParsed)
+        } catch (error) {
+            ctx.body = `subreddit ${subreddit} does not exist`
+        }
+    },
+    search_reddit_names: async(ctx) => {
+        try {
+            console.log('autcomplete suggestions...')
+            const res = await rp({
+                    uri: 'https://www.reddit.com/api/search_reddit_names.json',
+                    method: 'POST',
+                    form: { query: ctx.body.query }
+                })
+                // console.log(res)
+            ctx.body = res
+        } catch (error) {
+            ctx.body = error
+        }
     }
-  },
-  search_reddit_names: async(ctx) => {
-    try {
-      console.log('autcomplete suggestions...')
-      const res = await rp({
-          uri: 'https://www.reddit.com/api/search_reddit_names.json',
-          method: 'POST',
-          form: { query: ctx.body.query }
-        })
-        // console.log(res)
-      ctx.body = res
-    } catch (error) {
-      ctx.body = error
-    }
-  }
 }
+
 app.use(_.get('/', reddit.frontpage));
 app.use(_.post('/posts', reddit.posts))
 app.use(_.post('/search_reddit_names', reddit.search_reddit_names))
 app.use(_.get('/:subreddit', reddit.subreddit))
 app.use(_.get('/:subreddit/after/:afterCode', reddit.subredditNextPage))
-app.use(_.get('/:subreddit/before/:beforeCode', reddit.subredditPrevPage))
 
 app.listen(PORT);
 console.log(`Listening on port ${PORT}`)
